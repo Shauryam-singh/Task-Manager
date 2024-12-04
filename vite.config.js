408227d525973933970738815cdcf2eb02e71b30
shauryam-singh <shauryamsingh9@gmail.com>
2024-12-04 12:10:34 +0530

    Vite Config

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,4 @@ export default defineConfig({
     host: '0.0.0.0', // Bind to all interfaces
     port: parseInt(process.env.PORT) || 3000, // Use Render's port or default to 3000
   },
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 00cb5e2432ec27e2d09cefa231aeee3d27c67b63
